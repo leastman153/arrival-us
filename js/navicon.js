69b0de5a1f5704a60e3b3ee340d4f94927234c90
LEastman153 <leastman153@west-mec.org>
2018-05-10 13:19:03 -0700

    Revert "Merge branch 'testing'"
    This reverts commit 3f536522a83c661f3651c96809c8b7e160c28874, reversing
    changes made to 378237d1e56c2812cb4f77bd3b83f30b8a60f1c8.

--- js/navicon.js
+++ js/navicon.js
@@ -1,14 +1,16 @@
 $("document").ready(function() {
-  var navin = false;
-  $("#naviconcontainer").click(function() {
-    if(navin) {
-      $("#mobnavcontain").css("left", "100%");
-      $("#mobilenav div a").css("opacity", "0").css("transition", "1s");
-      navin = false;
+  var navopen = false;
+  $("#naviconcontainer").click(function(e) {
+    var navic = $("#naviconcontainer");
+    if(navopen) {
+      navic.css("width", "65px").css("background-color", "black");
+      $("#navicon").css("transform", "rotate(0deg)");
+      navopen = false;
     } else {
-      $("#mobnavcontain").css("display", "block").css("left", "0").css("transition", "left 1s, height 1s");
-      $("#mobilenav div a").css("opacity", "1").css("transition", "opacity 3s");
-      navin = true;
+      navic.css("width", "95%").css("background-color", "rgb(230, 230, 230, 95%)").css("transition", "width 1s, background-color 1s");
+      $("#naviconcontainer ul a").css("display", "flex");
+      $("#navicon").css("transform", "rotate(-180deg)").css("transition", "transform 1s");
+      navopen = true;
     }
   });
 });
