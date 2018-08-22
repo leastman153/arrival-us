abd7f4a59f7b7df71aaeecb4c02e88a234f335fb
Unknown <DMiller437@west-mec.org>
2018-08-22 15:30:12 -0700

    yi
    uhhhh

--- js/donate.js
+++ js/donate.js
@@ -1,104 +1,128 @@
-$("document").ready(function() {
-  $("#donate").click(validate);
-});
+//arrival-us website
+//
+//       Authors:Dylan Jones-Miller
+//       Date: 8.20.18
+//
+//Filename: donate.js
 
-function validate() {
-  // alert("Test 2");
-  var fName = document.getElementById("firstName");
-  if (fName.validity.valueMissing || fName.validity.patternMismatch) {
-      fName.setCustomValidity("Enter your name as it appears on your card.");
-  } else {
-      fName.setCustomValidity("");
-  }
-  var lName = document.getElementById("lastName");
-  if(lName.validity.valueMissing || lName.validity.patternMismatch) {
-    lName.setCustomValidity("Enter your name as it appears on your card.");
-  } else {
-    lName.setCustomValidity("");
-  }
-  var street = document.getElementById("street");
-  if(street.validity.valueMissing || street.validity.patternMismatch) {
-    street.setCustomValidity("Enter your full street address.");
-  } else {
-    street.setCustomValidity("");
-  }
-  var city = document.getElementById("city");
-  if(city.validity.valueMissing || city.validity.patternMismatch) {
-    city.setCustomValidity("Enter your city.");
-  } else {
-    city.setCustomValidity("");
-  }
-  var state = document.getElementById("state");
-  if(state.selectedIndex === 0) {
-    state.setCustomValidity("Please select your state.");
-  } else {
-    state.setCustomValidity("");
-  }
-  var zip = document.getElementById("zip");
-  if(zip.validity.valueMissing || zip.validity.patternMismatch) {
-    zip.setCustomValidity("Please enter your 5 digit zip code.");
-  } else {
-    zip.setCustomValidity("");
-  }
-  var cardNum = document.getElementById("cardNum");
-  if(cardNum.validity.valueMissing || cardNum.validity.patternMismatch || luhn(cardNum.value) == false) {
-    cardNum.setCustomValidity("Please enter your card number as it appears on your card.");
-  } else {
-    cardNum.setCustomValidity("");
-  }
-  var cardCVC = document.getElementById("cardCVC");
-  if(cardCVC.validity.valueMissing || cardCVC.validity.patternMismatch) {
-    cardCVC.setCustomValidity("Please enter your card CVC as it appears on your card.");
-  } else {
-    cardCVC.setCustomValidity("");
-  }
-  var cardType = document.getElementById("cardType");
-  if(cardType.selectedIndex === 0) {
-    cardType.setCustomValidity("Please select your card type.");
-  } else {
-    cardType.setCustomValidity("");
-  }
-  var expMonth = document.getElementById("expMonth");
-  if(expMonth.selectedIndex === 0) {
-    expMonth.setCustomValidity("Please select your card's expiration date.");
-  } else {
-    expMonth.setCustomValidity("");
-  }
-  var expYear = document.getElementById("expYear");
-  if(expYear.selectedIndex === 0) {
-    expYear.setCustomValidity("Please select your card's expiration year.");
-  } else {
-    expYear.setCustomValidity("");
-  }
-  var amount = document.getElementById("amount");
-  if(amount.selectedIndex === 0) {
-    amount.setCustomValidity("Please select your donation amount.");
-  } else {
-    amount.setCustomValidity("");
-  }
+var formValidity = true;
+
+function removeInputElements() {
+    var emptyBoxes = document.getElementsByTagName("input");
+     for (var i = 0; i < emptyBoxes.length; i++) {
+         emptyBoxes[i].selectedIndex = -1;
+     }
+    
 }
 
-function sumDigits(numStr) {
-    var digitTotal = 0;
-    for (var i = 0; i < numStr.length; i++) {
-        digitTotal += parseInt(numStr.charAt(i));
+//removing defaults for select list
+function removeSelectDefaults() {
+     var emptyBoxes = document.getElementsByTagName("select");
+     for (var i = 0; i < emptyBoxes.length; i++) {
+         emptyBoxes[i].selectedIndex = -1;
+     }
+ }
+
+
+function validateDeliveryDate() {
+    var inputElements = document.querySelectorAll(".formRow input");
+    var selectElements = document.querySelectorAll(".formRow select");
+    var errorDiv = document.getElementsByClassName("errorMessage");
+    var fieldsetValidity = true;
+    var elementCount = selectElements.length;
+    var inputCount = inputElements.length;
+    var currentElement;
+    try {
+
+        // loop through the select fields looking for blanks
+        for (var i = 0; i < elementCount; i++) {
+            currentElement = selectElements[i];
+            //blanks
+            if (currentElement.selectedIndex === -1) {
+                currentElement.style.border = "1px solid red";
+                fieldsetValidity = false;
+            }
+            //not blanks
+            else {
+                currentElement.style.background = "white";
+            }
+        }
+
+            //   for inputs
+            for (var i = 0; i < elementCount; i++) {
+                currentElement = inputElements[i];
+                //blanks
+                if (currentElement.selectedIndex === -1) {
+                    currentElement.style.border = "1px solid red";
+                    fieldsetValidity = false;
+                }
+                //not blanks
+                else {
+                    currentElement.style.background = "white";
+                }
+
+            }
+         // action for invalid field set
+        if (fieldsetValidity === false) {
+            throw "Please specify a Delivery Date."
+        } else {
+            errorDiv.style.display = "none";
+            errorDiv.innerHTML = "";
+        }
     }
-    return digitTotal;
+       
+     catch (msg) {
+        errorDiv.style.display = "block";
+        errorDiv.innerHTML = msg;
+        formValidity = false;
+    }
 }
 
-function luhn(idNum) {
-    var string1 = "";
-    var string2 = "";
-
-    // Retrieve the odd numbered digits
-    for (var i = idNum.length - 1; i >= 0; i -= 2) {
-        string1 += idNum.charAt(i);
+function validateForm(evt) {
+    //    prevent form default behavior submit
+    if (evt.preventDefault) {
+        evt.preventDefault();
+    } else {
+        evt.returnValue = false;
     }
-    // Retrieve the even numbered digits and double them
-    for (var i = idNum.length - 2; i >= 0; i -= 2) {
-        string2 += 2 * idNum.charAt(i);
-    }
+    formValidity = true;
+    validateDeliveryDate();
 
-    // Return whether the sume of the digits is divisible by 10
-    return sumDigits(string1 + string2) % 10 === 0;
+    if (formValidity === true) { //form is valid
+        document.getElementById("errorText").innerHTML = "";
+        document.getElementById("errorText").style.display = "none"
+        document.getElementsByTagName("form")[0].submit();
+    } else {
+        document.getElementById("errorText").innerHTML =
+            "Please fix the indicated problems and then resubmit your order.";
+        document.getElementById("errorText").style.display = "block";
+        scroll(0, 0);
+    }
+}
+
+
+//creating a function for event listeners
+//event handler will be validateForm
+function createEventListeners() {
+    var submit = document.getElementById("donate");
+    if (submit.addEventListener) {
+        submit.addEventListener("click", validateForm, false);
+    } else if (submit.attachEvent) {
+        submit.attachEvent("onclick", validateForm);
+    }
+}
+
+function setUpPage() {
+    removeSelectDefaults();
+    removeInputElements();
+    createEventListeners();
+}
+
+
+
+//page load event handlers
+if (window.addEventListener) {
+    window.addEventListener("load", setUpPage, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", setUpPage);
 }
